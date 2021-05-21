dfad62e23d1ff8c765363565ca071108223b9a1c
Lewin Probst <info@emirror.de>
2021-05-21 18:09:20 +0200

    Added binding generation files.

---
+++ sys/src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+extern crate gtk_sys;
+
+use gtk_sys::{
+    GtkContainer,
+    GtkContainerPrivate,
+    GtkMenu,
+    GtkMenuPrivate,
+    GtkMenuShell,
+    GtkMenuShellPrivate,
+    GtkStatusIcon,
+    GtkStatusIconPrivate,
+    GtkWidget,
+    GtkWidgetPrivate,
+};
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
